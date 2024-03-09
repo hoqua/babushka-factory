4ac887924ada5218709a974dfe63253e882c9941
Scribbletal'k <catreen02@gmail.com>
2024-03-09 16:20:54 +0200

    Добавил коллизию клешне с конвейером. Почистил код спавнера и анимации.

--- Assets/Scripts/Claw.cs
+++ Assets/Scripts/Claw.cs
@@ -1,36 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Claw : MonoBehaviour
 {
+    private Vector2 initialPosition;
     public float speed = 5f;
-    bool isMoving = false; // Флаг, указывающий, двигается ли объект в данный момент
-    Vector3 targetPosition; // Позиция, к которой объект должен двигаться
+    private Vector2 targetPosition; // Позиция, к которой объект должен двигаться
+    private MovingDirection? movingDirection = null;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isMoving) 
+        
+        if (Input.GetMouseButtonDown(0) && movingDirection == null) 
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            targetPosition.y = transform.position.y; // Блокируем изменение по y
-            targetPosition.z = transform.position.z; // Блокируем изменение по Z 
-
-            isMoving = true;
             
+            movingDirection = MovingDirection.Horizontal;
         }
-
-        if (isMoving)
+        
+        if (movingDirection == MovingDirection.Horizontal)
         {
-            
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+            MoveHorizontal();
+        }
+        if (movingDirection == MovingDirection.Down)
+        {
+            MoveDown();
+        }
+        if (movingDirection == MovingDirection.Up)
+        {
+            MoveUp();
+        }
+        
+    }
 
-            
-            if (transform.position == targetPosition)
-            {
-                isMoving = false; 
-            }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Conveyor"))
+        {
+            movingDirection = MovingDirection.Up;
         }
     }
+
+    void MoveHorizontal()
+    {
+        var horizontalTarget = new Vector2(targetPosition.x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, horizontalTarget, Time.deltaTime * speed);
+        
+        if (Math.Abs(transform.position.x - horizontalTarget.x) < 0.0001f)
+        {
+            movingDirection = MovingDirection.Down;
+        }
+    }
+
+    void MoveDown()
+    {
+        var verticalTarget = new Vector2(transform.position.x, targetPosition.y);
+        transform.position = Vector3.MoveTowards(transform.position, verticalTarget, Time.deltaTime * speed);
+        
+        if (Math.Abs(transform.position.y - verticalTarget.y) < 0.0001f)
+        {
+            movingDirection = MovingDirection.Up;
+        }
+    }
+    
+    void MoveUp()
+    {
+        var verticalTarget = new Vector2(transform.position.x, initialPosition.y);
+        transform.position = Vector3.MoveTowards(transform.position, verticalTarget, Time.deltaTime * speed);
+        
+        if (Math.Abs(transform.position.y - verticalTarget.y) < 0.0001f)
+        {
+            movingDirection = null;
+        }
+    }
+}
+
+
+enum MovingDirection
+{
+    Horizontal, 
+    Up,
+    Down
 }
