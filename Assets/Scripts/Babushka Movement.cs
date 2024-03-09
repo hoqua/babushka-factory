bc986933f287c74534652c052339045594993d72
Scribbletal'k <catreen02@gmail.com>
2024-03-09 16:20:54 +0200

    Добавил коллизию клешне с конвейером. Почистил код спавнера и анимации.

--- Assets/Scripts/Babushka Movement.cs
+++ Assets/Scripts/Babushka Movement.cs
@@ -1,26 +1,24 @@
-using System;
 using UnityEngine;
 
 public class BabushkaMovement : MonoBehaviour
 {
     public new Animator animation;
     private static readonly int IsPushed = Animator.StringToHash("isPushed");
-    public Rigidbody2D babushka;
 
     void Start()
     {
         animation = GetComponent<Animator>();
     } 
     
-    //Триггерит анимацию хождения когда бабушка задевает конвейер
-    private void OnTriggerEnter2D(Collider2D other)
+    //This triggers babushka's animations if she collides with conveyor
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Deleter"))
         {
             Destroy(GameObject.Find("Babushka Purple(Clone)"));
         }
         
-        if (other.CompareTag("Conveyor"))
+        else if (other.CompareTag("Conveyor"))
         {
             animation.SetBool(IsPushed, true);
         }
@@ -29,30 +27,8 @@ public class BabushkaMovement : MonoBehaviour
         {
             animation.SetBool(IsPushed, false);
         }
-        
+
         
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Claw"))
-        {
-            babushka = GetComponent<Rigidbody2D>();
-            babushka.isKinematic = true;
-            Debug.Log("Gotcha!");
-            animation.SetBool(IsPushed, false);
-            transform.parent = GameObject.Find("Claw").transform;
-            transform.parent.gameObject.SetActive(true);
-        }
-        
-    }
-    
-    //Отключает анимацию пока бабушка в клешне
-    private void OnCollisionStay2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Claw"))
-        {
-            animation.SetBool(IsPushed, false);
-        }
-    }
 }
