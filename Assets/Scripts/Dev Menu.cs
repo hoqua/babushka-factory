6555aa2e7a3047f8b76fe2a07e7a9ef3b310ce34
Scribbletal'k <catreen02@gmail.com>
2024-06-11 13:40:40 +0300

    Добавлен dropdown

--- Assets/Scripts/Dev Menu.cs
+++ Assets/Scripts/Dev Menu.cs
@@ -1,13 +1,31 @@
 using Game;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DevMenuController : MonoBehaviour
 {
-    public GameObject devMenuPanel; 
+    public GameObject devMenuPanel;
     private bool _isDevMenuVisible = false;
 
     public PlayerManager playerManager;
-
+    
+    public void CardUpgrade(int val)
+    {
+        if (val == 0)
+        {
+            Debug.Log("Опция 1");
+        }
+        
+        if (val == 1)
+        {
+            Debug.Log("Опция 2");
+        }
+        if (val == 2)
+        {
+            Debug.Log("Опция 3");
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F1))
@@ -16,6 +34,7 @@ public class DevMenuController : MonoBehaviour
         }
     }
 
+    
     public void LevelUp()
     {
         playerManager.currentExp = playerManager.requiredExp;
